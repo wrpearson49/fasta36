f086bca5dc17b74cfbbc024178ee599580e432d4
William R Pearson <wrp@virginia.edu>
2024-05-23 16:18:40 -0400

    update verstr to May, 2024

--- src/initfa.c
+++ src/initfa.c
@@ -522,9 +522,9 @@ char *iprompt1=" test sequence file name: ";
 char *iprompt2=" database file name: ";
 
 #ifdef PCOMPLIB
-char *verstr="36.3.8i Sept, 2023 MPI";
+char *verstr="36.3.8i May, 2024 MPI";
 #else
-char *verstr="36.3.8i Sept, 2023";
+char *verstr="36.3.8i May, 2024";
 #endif
 
 static int mktup=3;
